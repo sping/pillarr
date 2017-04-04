08ebf871e12018344079a30683e73a06f37e5833
Manuel van Rijn <manuel@manuelvanrijn.nl>
2017-04-04 15:23:00 +0200

    Split up passenger 4 and 5

--- lib/pillarr/plugins/passenger.rb
+++ lib/pillarr/plugins/passenger.rb
@@ -1,4 +1,3 @@
-# inspired by: https://github.com/scoutapp/scout-plugins/tree/master/passenger
 module Pillarr
   module Plugins
     class Passenger < Pillarr::Plugin
@@ -6,11 +5,16 @@ module Pillarr
         passenger_status_command: sudo /usr/bin/passenger-status
       EOS
 
+      needs 'rexml/document'
+
       def report_template
         {
+          version: 'unkown',
+          total_apps: 0,
           max_pool_size: 0,
-          process_current: 0,
-          queue_depth: 0
+          used_pool_size: 0,
+          wait_list_size: 0,
+          apps: []
         }
       end
 
@@ -21,32 +25,38 @@ module Pillarr
       private
 
       def collect_passenger_status
+        # since version: 5.0.7 we can use the xml containing more info
         cmd  = option(:passenger_status_command) || 'sudo /usr/bin/passenger-status'
-        data = `#{cmd} 2>&1`
+        data = `#{cmd} --show=xml 2>&1`
         unless $?.success?
           return error("Could not get data from command: #{cmd}. Error:  #{data}")
         end
 
-        # Passenger < 4 and passenger >= 4 report different stats. This loop extracts both formats.
-        data.each_line do |line|
-          if line =~ /^max\s+=\s(\d+)/
-            report(:max_pool_size, $1.to_i)
-          elsif line =~ /^count\s+=\s(\d+)/
-            report(:process_current, $1.to_i)
-          elsif line =~ /^active\s+=\s(\d+)/
-            report(:process_active, $1.to_i)
-          elsif line =~ /^inactive\s+=\s(\d+)/
-            report(:process_inactive, $1.to_i)
-          elsif line =~ /^Waiting on global queue: (\d+)/
-            report(:queue_depth, $1.to_i)
-          elsif line =~ /^Max pool size +: (\d+)/ # passenger 4
-            report(:max_pool_size, $1.to_i)
-          elsif line =~ /^Processes  +: (\d+)/   # passenger 4
-            report(:process_current, $1.to_i)
-          elsif line =~ /^\s+Requests in queue: (\d+)/  # passenger 4
-            report(:queue_depth, $1.to_i)
-          end
+        xml = REXML::Document.new(data)
+        version = xml.elements['info/passenger_version'].text rescue 'unkown'
+        total_apps = xml.elements['info/group_count'].text.to_i rescue 'unkown'
+        max_pool_size = xml.elements['info/max'].text.to_i rescue 0
+        used_pool_size = xml.elements['info/capacity_used'].text.to_i rescue 0
+        wait_list_size = xml.elements['info/get_wait_list_size'].text.to_i rescue 0
+
+        report(:version, version)
+        report(:total_apps, total_apps)
+        report(:max_pool_size, max_pool_size)
+        report(:used_pool_size, used_pool_size)
+        report(:wait_list_size, wait_list_size)
+
+        apps = {}
+        xml.root.each_element("/info/supergroups/*") do |e|
+          key = e.elements['group/app_root'].text[1..-1].gsub('/', '_')
+          apps[key] = {
+            path: e.elements['group/app_root'].text,
+            wait_list_size: e.elements['get_wait_list_size'].text.to_i,
+            used_pool_size: e.elements['capacity_used'].text.to_i
+          }
         end
+        report(:apps, apps)
+      rescue => e
+        error(e.message, e)
       end
     end
   end
